4912ff642f01de60bc6e690ef40d1c2f69b24101
Flavian Alexandru <alexandruflavian23@gmail.com>
2015-05-28 10:30:43 +0100

    Adding a table

--- phantom-migrations/src/main/scala/com/websudos/phantom/migrations/package.scala
+++ phantom-migrations/src/main/scala/com/websudos/phantom/migrations/package.scala
@@ -7,7 +7,7 @@ import scala.concurrent.ExecutionContext
 
 
 package object migrations {
-  implicit class TableMigrations[T <: CassandraTable[T, _], R](val table: CassandraTable[T, R]) extends AnyVal {
+  implicit class TableMigrations[T <: CassandraTable[T, R], R](val table: CassandraTable[T, R]) extends AnyVal {
     def automigrate()(implicit session: Session, keySpace: KeySpace, ec: ExecutionContext) = {
       Differ.automigrate(table)
     }
