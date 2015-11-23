6f9cf8b1f680a047f298da5fd54ba2f79c44ee2a
Flavian Alexandru <flavian.alexandru@anomaly42.com>
2015-11-23 01:55:43 +0000

    Bumping util version

--- project/Build.scala
+++ project/Build.scala
@@ -21,7 +21,7 @@ import sbt._
 
 object Build extends Build {
 
-  val UtilVersion = "0.9.11"
+  val UtilVersion = "0.9.12"
   val PhantomVersion = "1.16.0"
   val DatastaxDriverVersion = "2.2.0-rc3"
   val ScalaTestVersion = "2.2.4"
