16d207cbcff977b7e78bccb91bfe7eca19db05e4
Flavian Alexandru <flavian.alexandru.external@ynap.com>
2017-11-21 11:02:11 +0000

    Revert "Fixing docs and beginning work on materialised views."
    This reverts commit a005e86c3976535f95138a7976d7a26953170fc2.

--- phantom-autotables/src/main/scala/com/outworkers/phantom/autotables/package.scala
+++ phantom-autotables/src/main/scala/com/outworkers/phantom/autotables/package.scala
@@ -2,7 +2,7 @@
  * Copyright (C) 2012 - 2017 Outworkers, Limited. All rights reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * The contents of this file are proprietary and strictly confidential.
- * Written by Flavian Alexandru<flavian@outworkers.com>, 6/2017.
+ * Written by Flavian Alexandru<flavian@outworkers.co.uk>, 6/2017.
  */
 package com.outworkers.phantom
 
